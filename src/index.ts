5da18bd445af701ecc144094eabaf04dfefe3e42
Gustavo <gustavoburgerf@gmail.com>
2026-01-08 21:35:52 -0300

    feat: added code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { app } from "./server/app"
-
+import { app } from "./server/app";
 
 app.listen(3000, () => {
-    console.log("Server rodando na porta 3000")
-})
+  console.log("Server rodando na porta 3000");
+});
